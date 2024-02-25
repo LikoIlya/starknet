ce8e6dde1c12f187f3e8c16f9c0c036c3e4123d4
Illia Likhoshva <ilyaliko64@gmail.com>
2024-02-25 16:47:50 +0200

    Move proxy log

--- utils/proxy_rpc_client.py
+++ utils/proxy_rpc_client.py
@@ -10,6 +10,8 @@ class ProxyRPCClient(RpcHttpClient):
     def __init__(self, node_url: str, session: Optional[ClientSession] = None, proxy: str = None):
         super().__init__(node_url, session)
         self.proxy_url = proxy
+        if self.proxy_url:
+            logger.warning(f"Proxying via: {self.proxy_url}")
 
     async def _make_request(
             self,
@@ -19,10 +21,7 @@ class ProxyRPCClient(RpcHttpClient):
             params: dict,
             payload: dict,
     ) -> dict:
-        try:
-            if self.proxy_url:
-                logger.warning(f"Proxying via: {self.proxy_url}")
-            async with session.request(
+        async with session.request(
                     method=http_method.value,
                     url=address,
                     params=params,
@@ -32,7 +31,6 @@ class ProxyRPCClient(RpcHttpClient):
             ) as request:
                 await self.handle_request_error(request)
                 return await request.json(content_type=None)
-        except: raise
 
 
 class ProxyFullNodeClient(FullNodeClient):
